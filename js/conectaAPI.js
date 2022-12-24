e9f992c6ee053684ed4b0b963ea125ebd1c01c73
Carlos Eduardo <105376328+delfo2@users.noreply.github.com>
2022-12-24 11:53:29 -0300

    adicionando fetch com metodo post para envio de vídeos e colocando na zona de exportação do js para ser reutiluzado durante outro processo

--- js/conectaAPI.js
+++ js/conectaAPI.js
@@ -5,6 +5,24 @@ async function carregaDados () {
     return dadosTratados;
 }
 
+async function enviaDados (titulo, descricao, url, imagem) {
+    const dadosPrimarios = await fetch('http://localhost:3000/videos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: descricao,
+            url: url,
+            imagem: imagem
+        })
+    })
+    const dadosProcessados = dadosPrimarios.json();
+    return dadosProcessados;
+}
+
 export const conectaApi = {
-    carregaDados
+    carregaDados,
+    enviaDados
 }
